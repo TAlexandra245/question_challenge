7686cf3a49cfc056f0cfb0d246e5708e7d0db806
TAlexandra245 <tomaalexandra8@gmail.com>
2023-12-17 11:06:30 +0200

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     return (
         <>
             <h1> Quiz generator </h1>
+            <p> Warning! Once you click 'Submit your answers', you can not go back to your quiz</p>
             <ShowQuestions></ShowQuestions>
         </>
     );
--- src/quiz/ShowQuestions.tsx
+++ src/quiz/ShowQuestions.tsx
@@ -1,16 +1,21 @@
 import {quizArray} from "../data/quizArray";
-import {useState} from "react";
+import {FormEvent, useState} from "react";
 import QuizProps from '../data/QuizProps'
 
 const initialShuffledQuestions = [...quizArray].sort(() => Math.random() - 0.5);
 const allQuestions = initialShuffledQuestions.map(question => question.question);
 const duplicateQuestions = [...new Set(allQuestions)]
+
 export function ShowQuestions() {
+
     const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
     const [shuffledQuestions, setShuffledQuestions] = useState<QuizProps[]>(initialShuffledQuestions);
     const [score, setScore] = useState(0);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [uniqueQuestions, setUniqueQuestions] = useState<string []>(duplicateQuestions);
+    const [error, setError] = useState<string | null>(null);
+    const [showScore, setShowScore] = useState<boolean>(false);
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     const incrementIndex = () => {
         if (currentQuestionIndex < initialShuffledQuestions.length - 1) {
@@ -24,6 +29,9 @@ export function ShowQuestions() {
     }
     const currentQuestion = shuffledQuestions[currentQuestionIndex];
     const handleAnswerSelection = (answer: string) => {
+        if(!answer) {
+            setError('Please fill the question');
+        }
         setSelectedAnswer(answer);
 
         if (answer === currentQuestion.correctAnswer) {
@@ -31,25 +39,37 @@ export function ShowQuestions() {
         }
     };
 
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setFormSubmitted(true);
+        setShowScore(true);
+    }
+
     return (
         <div>
             <div>
                 <h2>{currentQuestion.question}</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     {currentQuestion.answersArray.map((answer, answerIndex) => (
                         <div key={answerIndex}>
                             <input type="radio" id={`answer_${answerIndex}`} name="answers" value={answer}
                                    checked={selectedAnswer === answer}
-                                   onChange={() => handleAnswerSelection(answer)}/>
+                                   onChange={(e) => handleAnswerSelection(answer)}
+                                   disabled={formSubmitted}
+                                   required/>
                             <label htmlFor={`answer_${answerIndex}`}> {answer}</label>
+                            {error && <p style={{color: 'red'}}>{error}</p>}
                         </div>
                     ))}
+                    <button type="submit" disabled={formSubmitted}>Submit your answers</button>
                 </form>
+
+                <button onClick={() => decrementIndex()} disabled={showScore}>Previous</button>
+                <button onClick={() => incrementIndex()} disabled={showScore}>Next</button>
             </div>
-            <p> Score : {score} </p>
+            {showScore && (<p> Your score : {score} </p>)}
             <p> Question : question {currentQuestionIndex + 1} / question {shuffledQuestions.length}</p>
-            <button onClick={() => decrementIndex()}>Previous</button>
-            <button onClick={() => incrementIndex()}>Next</button>
         </div>
     )
 }
